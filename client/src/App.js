9f430483abebacbfd9e08c4e71e5cff1ee3cfb2f
Troy mangrum <tmmangrum@gmail.com>
2020-06-03 20:50:24 -0500

    added routes to get movies on screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route, Switch} from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -26,11 +28,23 @@ const App = () => {
   };
 
   return (
+    
     <div>
+      
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      
+      <Switch>
+        <Route path="/movies/:id">
+          <Movie />
+        </Route>
+        <Route path="/">
+          <MovieList movieList={movieList}/>
+        </Route>
+        
+
+      </Switch>
     </div>
   );
 };
 
-export default App;
+export default App; 
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  const id = useParams().id;
   useEffect(() => {
-    const id = 1;
+    
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
